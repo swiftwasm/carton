22594ae10579c6faa768f4e0585665aa93d1de67
Max Desiatov <max@desiatov.com>
2020-06-23 21:40:17 +0100

    Update ArgumentParser dependency

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
   platforms: [.macOS(.v10_15)],
   dependencies: [
     .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.1"),
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.1.0")),
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.2.0")),
     .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMinor(from: "0.1.3")),
     .package(url: "https://github.com/broadwaylamb/OpenCombine.git", .upToNextMinor(from: "0.9.0")),
     .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "4.5.0")),
@@ -21,8 +21,8 @@ let package = Package(
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         .product(name: "AsyncHTTPClient", package: "async-http-client"),
-        .product(name: "Vapor", package: "vapor"),
         .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
+        .product(name: "Vapor", package: "vapor"),
         "OpenCombine",
       ]
     ),
