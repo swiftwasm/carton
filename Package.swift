327015f4a352b690263d46f5b69a03bd379ecf5d
Max Desiatov <max@desiatov.com>
2020-07-08 13:44:34 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,9 @@ let package = Package(
     .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "4.5.0")),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    // Targets are the basic building blocks of a package. A target can define a module
+    // or a test suite. Targets can depend on other targets in this package, and on
+    // products in packages which this package depends on.
     .target(
       name: "carton",
       dependencies: [
