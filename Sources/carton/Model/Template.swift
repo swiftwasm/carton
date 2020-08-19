fd36a5fcc9988e3702301d28d8835ef505b4aefc
Max Desiatov <max@desiatov.com>
2020-08-19 19:45:55 +0100

    Add a trailing newline to the Tokamak template

--- Sources/carton/Model/Template.swift
+++ Sources/carton/Model/Template.swift
@@ -204,6 +204,7 @@ extension Templates {
         // @main attribute is not supported in SwiftPM apps.
         // See https://bugs.swift.org/browse/SR-12683 for more details.
         TokamakApp.main()
+
         """
         .write(to: $0)
       }
