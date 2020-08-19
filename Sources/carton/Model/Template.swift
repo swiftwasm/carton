aa0472844f6f6406a76b39c9561c935154a1bba8
Max Desiatov <max@desiatov.com>
2020-08-19 10:23:06 +0100

    Add `main()` function call to the Tokamak template

--- Sources/carton/Model/Template.swift
+++ Sources/carton/Model/Template.swift
@@ -200,6 +200,10 @@ extension Templates {
                 Text("Hello, world!")
             }
         }
+
+        // @main attribute is not supported in SwiftPM apps.
+        // See https://bugs.swift.org/browse/SR-12683 for more details.
+        TokamakApp.main()
         """
         .write(to: $0)
       }
