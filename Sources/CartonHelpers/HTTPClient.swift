9982f4297863698ad2735c36792240ba957ad5a8
Max Desiatov <max@desiatov.com>
2020-09-27 20:22:44 +0100

    Apply token to GH API requests, install macOS deps

--- Sources/CartonHelpers/HTTPClient.swift
+++ Sources/CartonHelpers/HTTPClient.swift
@@ -23,7 +23,10 @@ public extension HTTPClient.Request {
   static func get(url: String) throws -> Self {
     var request = try HTTPClient.Request(url: url)
     request.headers.add(name: "User-Agent", value: "carton \(cartonVersion)")
-    if let token = ProcessInfo.processInfo.environment["GITHUB_TOKEN"] {
+    if
+      url.starts(with: "https://api.github.com/"),
+      let token = ProcessInfo.processInfo.environment["GITHUB_TOKEN"]
+    {
       request.headers.add(name: "Authorization", value: "Bearer \(token)")
     }
     return request
