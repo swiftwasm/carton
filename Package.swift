a1cd7f2dbac2c8469ed8017c09cf741ddc3f85d2
Carson Katri <carson.katri@gmail.com>
2020-10-08 09:53:20 -0400

    Add '.git' to Splash

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.10.0"),
     .package(url: "https://github.com/vapor/vapor.git", from: "4.29.3"),
     .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.0"),
-    .package(url: "https://github.com/JohnSundell/Splash", .exact("0.13.0")),
+    .package(url: "https://github.com/JohnSundell/Splash.git", from: "0.14.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module
