3e6051fda7d67c6e94e317c0ae0da0bc0eb8f111
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-26 10:58:57 +0900

    Update default toolchain

--- Sources/CartonHelpers/DefaultToolchain.swift
+++ Sources/CartonHelpers/DefaultToolchain.swift
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-public let defaultToolchainVersion = "wasm-5.3-SNAPSHOT-2020-10-21-a"
+public let defaultToolchainVersion = "wasm-5.3-SNAPSHOT-2020-10-25-a"
