0ee05ba7a97879f4e49cc42eaee975a589600dba
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-31 16:57:52 +0900

    Update toolchain and remove extra compiler flags (#147)
    * Update toolchain version and remove extra compiler flags
    
    * Ignore .swiftpm

--- Sources/SwiftToolchain/Toolchain.swift
+++ Sources/SwiftToolchain/Toolchain.swift
@@ -223,7 +223,7 @@ public final class Toolchain {
 
     let builderArguments = [
       swiftPath.pathString, "build", "-c", isRelease ? "release" : "debug", "--product", product,
-      "--enable-test-discovery", "--triple", "wasm32-unknown-wasi", "-Xswiftc", "-static-stdlib",
+      "--enable-test-discovery", "--triple", "wasm32-unknown-wasi",
     ]
 
     try Builder(arguments: builderArguments, mainWasmPath: mainWasmPath, fileSystem, terminal)
@@ -254,7 +254,7 @@ public final class Toolchain {
 
     let builderArguments = [
       swiftPath.pathString, "build", "-c", isRelease ? "release" : "debug", "--build-tests",
-      "--enable-test-discovery", "--triple", "wasm32-unknown-wasi", "-Xswiftc", "-static-stdlib",
+      "--enable-test-discovery", "--triple", "wasm32-unknown-wasi",
       "-Xswiftc", "-color-diagnostics",
     ]
 
