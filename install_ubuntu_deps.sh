e075d1d9f500e1a13d5149f926216c5f8e09cc15
Yuta Saito <kateinoigakukun@gmail.com>
2020-11-06 23:12:42 +0900

    Remove wabt dependency

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-curl -L -v -o wabt.tar.gz https://github.com/WebAssembly/wabt/releases/download/1.0.19/wabt-1.0.19-ubuntu.tar.gz
-tar xzvf wabt.tar.gz
-cp wabt-1.0.19/bin/* /usr/local/bin
-
 curl -L -v -o binaryen.tar.gz https://github.com/WebAssembly/binaryen/releases/download/version_97/binaryen-version_97-x86_64-linux.tar.gz
 tar xzvf binaryen.tar.gz
 cp binaryen-version_97/bin/* /usr/local/bin
