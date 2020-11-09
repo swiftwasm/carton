50c8df05f15809bef6c9eacb0b7b14f138ee4375
Yuta Saito <kateinoigakukun@gmail.com>
2020-11-09 21:10:48 +0900

    Add minimum deployment target in template

--- Sources/carton/Model/Template.swift
+++ Sources/carton/Model/Template.swift
@@ -86,16 +86,22 @@ extension Template {
   static func createManifest(
     fileSystem: FileSystem,
     project: Project,
+    platforms: [String] = [],
     dependencies: [PackageDependency] = [],
     targetDepencencies: [TargetDependency] = [],
     _ terminal: InteractiveWriter
   ) throws {
     try fileSystem.writeFileContents(project.path.appending(component: "Package.swift")) {
-      """
+      var content = """
       // swift-tools-version:5.3
       import PackageDescription
       let package = Package(
-          name: "\(project.name)",
+          name: "\(project.name)",\n
+      """
+      if !platforms.isEmpty {
+        content += "    platforms: [\(platforms.joined(separator: ",\n"))],\n"
+      }
+      content += """
           products: [
               .executable(name: "\(project.name)", targets: ["\(project.name)"])
           ],
@@ -114,7 +120,7 @@ extension Template {
           ]
       )
       """
-      .write(to: $0)
+      content.write(to: $0)
     }
   }
 }
@@ -168,6 +174,7 @@ extension Templates {
       try createManifest(
         fileSystem: fileSystem,
         project: project,
+        platforms: [".macOS(.v10_15)"],
         dependencies: [
           .init(
             name: "Tokamak",
