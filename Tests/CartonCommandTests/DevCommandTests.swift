6772a5de97be3980a6b48acc50c84608b7ebcbc4
Max Desiatov <max@desiatov.com>
2021-05-24 11:24:10 +0100

    Clean up debug logging

--- Tests/CartonCommandTests/DevCommandTests.swift
+++ Tests/CartonCommandTests/DevCommandTests.swift
@@ -45,7 +45,6 @@ final class DevCommandTests: XCTestCase {
 
     guard let process = executeCommand(
       command: "carton dev --verbose",
-      shouldPrintOutput: true,
       cwd: packageDirectory.url
     ) else {
       XCTFail("Could not create process")
@@ -74,7 +73,6 @@ final class DevCommandTests: XCTestCase {
 
     guard let process = executeCommand(
       command: "carton dev --verbose --port 8081 --host 0.0.0.0",
-      shouldPrintOutput: true,
       cwd: packageDirectory.url
     ) else {
       XCTFail("Could not create process")
