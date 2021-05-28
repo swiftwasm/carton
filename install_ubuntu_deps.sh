2974e4b68d817512e778169ab788730abef750f1
Max Desiatov <max@desiatov.com>
2021-05-28 17:02:27 +0100

    Avoid using `sudo` if it's not installed

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -2,7 +2,11 @@
 
 set -ex
 
-sudo apt-get install zlib1g-dev
+if [ -x "$(command -v sudo)" ]; then
+  sudo apt-get install zlib1g-dev
+else
+  apt-get install zlib1g-dev
+fi
 curl -L -v -o binaryen.tar.gz https://github.com/WebAssembly/binaryen/releases/download/version_97/binaryen-version_97-x86_64-linux.tar.gz
 tar xzvf binaryen.tar.gz
 cp binaryen-version_97/bin/* /usr/local/bin
