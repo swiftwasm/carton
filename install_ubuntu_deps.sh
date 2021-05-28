10c21db4bfc84ae412f3d81bd9e2354149612fda
Max Desiatov <max@desiatov.com>
2021-05-28 22:00:38 +0100

    Fix Ubuntu deps, clarify Linux support in README (#242)
    Resolves #237.
    
    * Fix Ubuntu deps, clarify Linux support in README
    
    * Avoid using `sudo` if it's not installed

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+if [ -x "$(command -v sudo)" ]; then
+  sudo apt-get install zlib1g-dev
+else
+  apt-get install zlib1g-dev
+fi
 curl -L -v -o binaryen.tar.gz https://github.com/WebAssembly/binaryen/releases/download/version_97/binaryen-version_97-x86_64-linux.tar.gz
 tar xzvf binaryen.tar.gz
 cp binaryen-version_97/bin/* /usr/local/bin
