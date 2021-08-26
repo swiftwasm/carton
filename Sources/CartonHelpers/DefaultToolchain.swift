813f43f4a5b27989eafb1bc0f043e73a32b33081
Max Desiatov <max@desiatov.com>
2021-08-26 14:27:50 +0100

    Test with `wasm-5.4-SNAPSHOT-2021-08-26-a`

--- Sources/CartonHelpers/DefaultToolchain.swift
+++ Sources/CartonHelpers/DefaultToolchain.swift
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-public let defaultToolchainVersion = "wasm-5.4.0-RELEASE"
+public let defaultToolchainVersion = "wasm-5.4-SNAPSHOT-2021-08-26-a"
