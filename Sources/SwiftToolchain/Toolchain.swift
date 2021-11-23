9fb73de45dfb67e77e355ef7272a05e014ce2b3d
yonihemi <jonathan@hemi.dev>
2021-11-23 12:03:57 +0800

    Update JavaScriptKit to v0.11.1

--- Sources/SwiftToolchain/Toolchain.swift
+++ Sources/SwiftToolchain/Toolchain.swift
@@ -19,7 +19,7 @@ import TSCBasic
 import TSCUtility
 import WasmTransformer
 
-public let compatibleJSKitVersion = Version(0, 10, 1)
+public let compatibleJSKitVersion = Version(0, 11, 1)
 
 enum ToolchainError: Error, CustomStringConvertible {
   case directoryDoesNotExist(AbsolutePath)
