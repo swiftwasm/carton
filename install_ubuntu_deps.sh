569838aa6fdb4f2482a65ba56d19ddf7e531d1ba
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-01-17 17:34:59 +0100

    Upgrade binaryen binary version

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -7,6 +7,9 @@ if [ -x "$(command -v sudo)" ]; then
 else
   apt-get install zlib1g-dev
 fi
-curl -L -v -o binaryen.tar.gz https://github.com/WebAssembly/binaryen/releases/download/version_97/binaryen-version_97-x86_64-linux.tar.gz
+
+BINARYEN_VERSION=105
+
+curl -L -v -o binaryen.tar.gz https://github.com/WebAssembly/binaryen/releases/download/version_${BINARYEN_VERSION}/binaryen-version_${BINARYEN_VERSION}-x86_64-linux.tar.gz
 tar xzvf binaryen.tar.gz
-cp binaryen-version_97/bin/* /usr/local/bin
+cp binaryen-version_${BINARYEN_VERSION}/bin/* /usr/local/bin
