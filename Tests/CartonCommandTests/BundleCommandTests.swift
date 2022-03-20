dab930bc6cb056e8bf3e9404bb9b8c3a14ccbfb7
Yuta Saito <kateinoigakukun@gmail.com>
2022-03-20 02:08:04 +0000

    Add brief test suite for -Xswiftc

--- Tests/CartonCommandTests/BundleCommandTests.swift
+++ Tests/CartonCommandTests/BundleCommandTests.swift
@@ -57,4 +57,24 @@ final class BundleCommandTests: XCTestCase {
     try bundleDirectory.delete()
     try packageDirectory.appending(component: ".build").delete()
   }
+
+  func testWithXswiftc() throws {
+    let package = "Milk"
+    let packageDirectory = testFixturesDirectory.appending(component: package)
+
+    let bundle = "Bundle"
+    let bundleDirectory = packageDirectory.appending(component: bundle)
+
+    // it's ok if there is nothing to delete
+    do { try bundleDirectory.delete() } catch {}
+
+    AssertExecuteCommand(
+      command: "carton bundle -Xswiftc --fake-swiftc-options",
+      cwd: packageDirectory.url,
+      expected: "error: unknown argument: '--fake-swiftc-options'",
+      expectedContains: true
+    )
+
+    try packageDirectory.appending(component: ".build").delete()
+  }
 }
