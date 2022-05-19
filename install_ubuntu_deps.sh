2b4f6b9d34656fe1072cd3c6b624112c9af78d55
Max Desiatov <max@desiatov.com>
2022-05-19 21:12:15 +0100

    Update install_ubuntu_deps.sh

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -3,9 +3,9 @@
 set -ex
 
 if [ -x "$(command -v sudo)" ]; then
-  sudo apt-get install zlib1g-dev libsqlite3-dev libcurl-dev
+  sudo apt-get install zlib1g-dev libsqlite3-dev libcurl4-openssl-dev
 else
-  apt-get install zlib1g-dev libsqlite3-dev libcurl-dev
+  apt-get install zlib1g-dev libsqlite3-dev libcurl4-openssl-dev
 fi
 
 BINARYEN_VERSION=105
