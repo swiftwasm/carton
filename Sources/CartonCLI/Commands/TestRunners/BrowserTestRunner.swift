0fd008417d78526e564a950b82a22eba4516e7f9
Max Desiatov <max@desiatov.com>
2022-08-11 17:34:43 +0100

    Use `try?` to appease the linter

--- Sources/CartonCLI/Commands/TestRunners/BrowserTestRunner.swift
+++ Sources/CartonCLI/Commands/TestRunners/BrowserTestRunner.swift
@@ -134,7 +134,7 @@ struct BrowserTestRunner: TestRunner {
   }
 
   func run() async throws {
-    defer { try! httpClient.syncShutdown() }
+    defer { try? httpClient.syncShutdown() }
     try Constants.entrypoint.check(on: localFileSystem, terminal)
     let server = try await Server(
       .init(
