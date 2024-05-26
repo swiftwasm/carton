c63bb8a96e550623a29694a94829e6b105eff623
omochimetaru <omochi.metaru@gmail.com>
2024-05-26 15:44:54 +0900

    import Foundation

--- Sources/WebDriver/URLSessionAsync.swift
+++ Sources/WebDriver/URLSessionAsync.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Foundation
+
 #if canImport(FoundationNetworking)
 
 import FoundationNetworking
