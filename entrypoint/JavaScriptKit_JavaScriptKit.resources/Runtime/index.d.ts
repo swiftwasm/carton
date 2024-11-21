c5904e3b93dc3a3a5659137fe854015f03752fae
Yuta Saito <kateinoigakukun@gmail.com>
2024-11-21 19:17:36 +0900

    Use real http server for browser testing (#506)

--- entrypoint/JavaScriptKit_JavaScriptKit.resources/Runtime/index.d.ts
+++ entrypoint/JavaScriptKit_JavaScriptKit.resources/Runtime/index.d.ts
@@ -1,4 +1,5 @@
 export class SwiftRuntime {
+  constructor({ sharedMemory: booleam });
   setInstance(instance: WebAssembly.Instance): void;
   main?(): void;
   readonly wasmImports: ImportedFunctions;
